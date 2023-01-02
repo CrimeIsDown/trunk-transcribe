eb74d7b34978c49900e3734bd7b4bcf7192be56d
Eric Tendian <erictendian@gmail.com>
2023-01-02 16:19:54 -0600

    Hide base64 from flower dashboard

---
+++ flowerconfig.py
@@ -0,0 +1,6 @@
+import re
+
+def format_task(task):
+    if task.name == "transcribe":
+        task.kwargs = re.sub(r"'audio_file_b64': '(.*)'", "'audio_file_b64': '...'", task.kwargs)
+    return task
