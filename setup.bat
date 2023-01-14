ae99f20aad538cce0186cc5675ea4bf994f71fb7
Eric Tendian <erictendian@gmail.com>
2023-01-13 23:18:34 -0600

    Allow setup.bat to be used for updates too

--- setup.bat
+++ setup.bat
@@ -1,3 +1,4 @@
+git pull
 call .\.venv\Scripts\activate
 pip install --extra-index-url https://download.pytorch.org/whl/cu117 git+https://github.com/openai/whisper.git
 pip install -r requirements.txt
