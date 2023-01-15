abb97e03a40e434d81fbdf25bb00468d0264aee1
Eric Tendian <erictendian@gmail.com>
2023-01-14 21:30:04 -0600

    Fixing various bugs in start-vast script

--- start-vast.sh
+++ start-vast.sh
@@ -13,9 +13,14 @@ if [[ -z "$END" ]]; then
     START=1
 fi
 
-EXISTING_INSTANCES="$(vast show instances --raw | jq -r '.[].machine_id' | paste -sd\| -)"
+SHOW_INSTANCES_OUTPUT=$(mktemp)
+vast show instances --raw > $SHOW_INSTANCES_OUTPUT
+
+EXISTING_INSTANCES="$(jq -r '.[].machine_id' $SHOW_INSTANCES_OUTPUT | paste -sd\| -)"
 
 if [[ "$START" == "--min-instances" ]]; then
+    jq -r '.[] | select(.actual_status == "exited") | .id' $SHOW_INSTANCES_OUTPUT | xargs -r -n 1 vast destroy instance
+
     DESIRED_COUNT=$END
     CURRENT_COUNT=$(curl --fail-with-body -Ss 'http://crimeisdown:5555/api/workers?refresh=true&status=true' | jq -r 'to_entries | map(select(.value == true)) | length')
 
@@ -23,7 +28,9 @@ if [[ "$START" == "--min-instances" ]]; then
     END=$(($DESIRED_COUNT - $CURRENT_COUNT + 1))
 
     if [ $END -lt 2 ]; then
-        echo "Enough instances running, exiting"
+        if [ -t 1 ]; then
+            echo "Enough instances running, exiting"
+        fi
         exit
     fi
 fi
@@ -35,11 +42,14 @@ fi
 echo -e "Planning to start these instances:\n"
 
 INSTANCES=$(mktemp)
-QUERY="rentable=true rented=false reliability>0.98 num_gpus=1 dlperf_usd>200 dph<=0.1 cuda_vers>=11.7"
+QUERY="rentable=true rented=false reliability>0.98 num_gpus=1 gpu_ram>8 dlperf_usd>200 dph<=0.1 cuda_vers>=11.7"
 vast search offers -n -i -o 'dph' "$QUERY" | \
 grep -Ev "\b$EXISTING_INSTANCES\b" | \
 sed -n "1,1p;$((${START} + 1)),$((${END} + 1))p;$((${END} + 2))q" | tee $INSTANCES
-sleep 5
+
+if [ -t 1 ]; then
+    sleep 5
+fi
 
 cat $INSTANCES | \
 tr -s ' ' | \
@@ -59,33 +69,37 @@ do
     IMAGE="crimeisdown/trunk-transcribe:main-medium.en-cu117"
     REQUEST=$(mktemp)
 
-    set -x
-
-    tee $REQUEST << EOF
+    cat << EOF > $REQUEST
 {
     "client_id": "me",
     "image": "$IMAGE",
     "args": ["worker"],
     "env": {
         "CELERY_HOSTNAME": "celery-$(git rev-parse --short HEAD)@vast-$INSTANCE_ID",
-        "TELEGRAM_BOT_TOKEN": "$TELEGRAM_BOT_TOKEN",
-        "CELERY_BROKER_URL": "$CELERY_BROKER_URL",
-        "CELERY_RESULT_BACKEND": "$CELERY_RESULT_BACKEND",
-        "API_BASE_URL": "$API_BASE_URL",
-        "API_KEY": "$API_KEY",
-        "MEILI_URL": "$MEILI_URL",
-        "MEILI_MASTER_KEY": "$MEILI_MASTER_KEY",
-        "S3_ENDPOINT": "$S3_ENDPOINT",
-        "S3_PUBLIC_URL": "$S3_PUBLIC_URL",
-        "AWS_ACCESS_KEY_ID": "$AWS_ACCESS_KEY_ID",
-        "AWS_SECRET_ACCESS_KEY": "$AWS_SECRET_ACCESS_KEY",
-        "S3_BUCKET": "$S3_BUCKET"
+EOF
+
+    while read line; do
+        KEY="$(echo "$line" | cut -d= -f1)"
+        VALUE="$(echo "$line" | cut -d= -f2-)"
+        echo "        \"$KEY\": \"$VALUE\"," >> $REQUEST
+    done < .env.vast
+    # Remove the trailing comma
+    head -c -2 $REQUEST > $REQUEST.new
+    # Add back the newline
+    echo >> $REQUEST.new
+    mv $REQUEST.new $REQUEST
+
+    cat << EOF >> $REQUEST
     },
     "price": $BID,
     "disk": 0.5,
     "runtype": "args"
 }
 EOF
+
+    cat $REQUEST
+
+    set -x
 
     curl -s --fail-with-body --request PUT \
         --url "https://console.vast.ai/api/v0/asks/$INSTANCE_ID/?api_key=$(cat ~/.vast_api_key)" \
@@ -98,4 +112,6 @@ done
 
 rm $INSTANCES
 
-watch -n 5 vast show instances
+if [ -t 1 ]; then
+    watch -n 5 -w vast show instances
+fi
