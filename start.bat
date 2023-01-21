d8fac922c39e6da9a024b52ca0ab28a18fef6e48
Eric Tendian <erictendian@gmail.com>
2023-01-21 15:28:56 -0600

    Have Windows listen on transcribe queue

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 call .\.venv\Scripts\activate
 for /f %%i in ('git rev-parse --short HEAD') do set GIT_COMMIT=%%i
-celery --app=app.worker.celery worker --loglevel=info -c 1 -P gevent -n celery-%GIT_COMMIT%@%%n
+celery --app=app.worker.celery worker --loglevel=info -c 1 -P gevent -n celery-%GIT_COMMIT%@%%n -Q transcribe
