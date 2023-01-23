f261133dbd6296ce8d05420dc0340d83fb07e9a6
Eric Tendian <erictendian@gmail.com>
2023-01-23 13:41:39 -0600

    Improving alert messages

--- app/notification.py
+++ app/notification.py
@@ -24,15 +24,19 @@ def get_config(metadata: Metadata) -> ChannelConfig | None:
     return None
 
 
-def prep_transcript(metadata: Metadata, transcript: str, channel: ChannelConfig):
+def truncate_transcript(transcript: str) -> str:
     # Telegram has a 1024 char max for the caption, so truncate long ones
     # (we use less than 1024 to account for HTML and what we will add next)
     transcript_max_len = 950
     if len(transcript) > transcript_max_len:
         transcript = f"{transcript[:transcript_max_len]}... (truncated)"
+    return transcript
 
-    if channel["append_talkgroup"]:
-        transcript = transcript + f"\n<b>{metadata['talkgroup_tag']}</b>"
+
+def build_suffix(metadata: Metadata, add_talkgroup: bool = False) -> str:
+    suffix = []
+    if add_talkgroup:
+        suffix.append(f"<b>{metadata['talkgroup_tag']}</b>")
 
     # If delayed by over 2 mins add delay warning
     if time() - metadata["stop_time"] > 120:
@@ -43,9 +47,9 @@ def prep_transcript(metadata: Metadata, transcript: str, channel: ChannelConfig)
             .astimezone(pytz.timezone(os.getenv("TZ", "America/Chicago")))
             .strftime(windows_format if platform == "win32" else linux_format)
         )
-        transcript = transcript + f"\n\n<i>{timestamp} (delayed)</i>"
+        suffix.append(f"\n<i>{timestamp} (delayed)</i>")
 
-    return transcript
+    return "\n".join(suffix)
 
 
 def send_notifications(
@@ -69,7 +73,8 @@ def send_notifications(
 
     voice_file = convert_to_ogg(audio_file=audio_file)
 
-    transcript = prep_transcript(metadata, transcript, channel)
+    transcript = truncate_transcript(transcript)
+    suffix = build_suffix(metadata, channel["append_talkgroup"])
 
     # Monkey patch NotifyTelegram so we can send voice messages with captions
     NotifyTelegramBase.send = NotifyTelegram.send  # type: ignore
@@ -79,27 +84,55 @@ def send_notifications(
 
     notifier.add(f"tgram://{bot_token}/{channel['chat_id']}")
 
-    notifier.notify(body=transcript, attach=voice_file)  # type: ignore
+    notifier.notify(body="\n".join([transcript, suffix]), attach=voice_file)  # type: ignore
 
     for alert_chat_id, alert_keywords in channel["alerts"].items():
-        matched_keywords = [
-            keyword
-            for keyword in alert_keywords
-            if keyword.lower() in transcript.lower()
+        send_alert(
+            alert_chat_id,
+            alert_keywords,
+            metadata,
+            transcript,
+            channel,
+            raw_audio_url,
+            bot_token,
+        )
+
+
+def send_alert(
+    alert_chat_id: str,
+    alert_keywords: list[str],
+    metadata: Metadata,
+    transcript: str,
+    channel: ChannelConfig,
+    raw_audio_url: str,
+    bot_token: str,
+):
+    # If we haven't already appended the talkgroup, do it for the alert
+    suffix = build_suffix(metadata, add_talkgroup=True)
+
+    matched_keywords = []
+    matched_lines = []
+    for line in transcript.splitlines():
+        matches = [
+            keyword for keyword in alert_keywords if keyword.lower() in line.lower()
         ]
-        if len(matched_keywords):
-            notifier = Apprise()
-            notifier.add(f"tgram://{bot_token}/{alert_chat_id}")
+        if len(matches):
+            matched_keywords += matches
+            matched_lines.append(line)
 
-            body = (
-                "<u>"
-                + ", ".join(matched_keywords)
-                + " detected in transcript</u>\n"
-                + transcript
-            )
-            # If we haven't already appended the talkgroup, do it for the alert
-            if not channel["append_talkgroup"]:
-                body = body + f"\n<b>{metadata['talkgroup_tag']}</b>"
-            body = body + f'\n<a href="{raw_audio_url}">Listen to transmission</a>'
+    if len(matched_keywords):
+        notifier = Apprise()
+        notifier.add(f"tgram://{bot_token}/{alert_chat_id}")
 
-            notifier.notify(body=body)
+        body = "<u>" + ", ".join(matched_keywords) + " detected in transcript</u>\n"
+
+        # Avoid duplicating the transcript if we don't have to
+        transcript_excerpt = "\n".join(matched_lines)
+        if transcript_excerpt == transcript:
+            body += transcript
+        else:
+            body += transcript_excerpt + "\n---\n" + transcript
+
+        body += f'\n{suffix}\n<a href="{raw_audio_url}">Listen to transmission</a>'
+
+        notifier.notify(body=body)
