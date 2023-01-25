b46eda76b50adfdee459987f19f4ad8868d39d9f
Eric Tendian <erictendian@gmail.com>
2023-01-24 19:46:39 -0600

    Remove dependency on Vast CLI

--- autoscale-vast.py
+++ autoscale-vast.py
@@ -16,6 +16,7 @@ from dotenv import dotenv_values
 from requests.auth import HTTPBasicAuth
 
 past_utilization = []
+vast_api_key = None
 
 
 def get_queue_status(envs: dict) -> dict:
@@ -35,7 +36,7 @@ def get_queue_status(envs: dict) -> dict:
 @lru_cache()
 def get_git_commit() -> str:
     if os.getenv("GIT_COMMIT"):
-        return os.environ["GIT_COMMIT"]
+        return os.environ["GIT_COMMIT"][:7]
     p = subprocess.run(
         [
             "git",
@@ -50,121 +51,109 @@ def get_git_commit() -> str:
 
 
 def find_available_instances() -> list[dict]:
-    query = [
-        "rentable=true",
-        "rented=false",
-        "reliability>0.98",
-        "num_gpus=1",
-        "gpu_ram>=8",
-        "dlperf_usd>200",
-        "dph<=0.1",
-        "cuda_vers>=11.7",
-        "inet_up>=90",
-        "inet_down>=90",
-    ]
+    query = {
+        "rentable": {"eq": "true"},
+        "rented": {"eq": "false"},
+        "reliability": {"gt": "0.98"},
+        "reliability2": {"gt": "0.98"},
+        "num_gpus": {"eq": "1"},
+        "gpu_ram": {"gt": "8000.0"},
+        "dlperf_usd": {"gt": "200"},
+        "dlperf_per_dphtotal": {"gt": "200"},
+        "dph": {"lte": "0.1"},
+        "dph_total": {"lte": "0.1"},
+        "cuda_vers": {"gte": "11.7"},
+        "cuda_max_good": {"gte": "11.7"},
+        "inet_up": {"gte": "90"},
+        "inet_down": {"gte": "90"},
+        "order": [["dph_total", "asc"]],
+        "type": "bid",
+    }
 
-    p = subprocess.run(
-        [
-            "vast",
-            "search",
-            "offers",
-            "--raw",
-            "-n",  # Disable default query
-            "-i",  # Interruptible
-            "-o",  # Order by...
-            "dph",  # $/hr
-            " ".join(query),
-        ],
-        capture_output=True,
+    r = requests.get(
+        "https://console.vast.ai/api/v0/bundles",
+        params={"q": json.dumps(query), "api_key": vast_api_key},
     )
-    p.check_returncode()
-    try:
-        return json.loads(p.stdout)
-    except json.decoder.JSONDecodeError as e:
-        logging.error(p.stdout)
-        raise e
+    r.raise_for_status()
+    return r.json()["offers"]
 
 
 def get_current_instances() -> list[dict]:
-    p = subprocess.run(
-        [
-            "vast",
-            "show",
-            "instances",
-            "--raw",
-        ],
-        capture_output=True,
+    r = requests.get(
+        "https://console.vast.ai/api/v0/instances",
+        params={"owner": "me", "api_key": vast_api_key},
     )
-    p.check_returncode()
-    try:
-        return json.loads(p.stdout)
-    except json.decoder.JSONDecodeError as e:
-        logging.error(p.stdout)
-        return []
+    r.raise_for_status()
+    return r.json()["instances"]
 
 
-def create_instance(envs: dict, image: str) -> bool:
-    instance = find_available_instances()[0]
+def create_instances(count: int, envs: dict, image: str):
+    instances = find_available_instances()
 
-    instance_id = instance["id"]
-    # Bid 1.5x the minimum bid
-    bid = float(instance["dph_total"]) * 1.5
+    while count and len(instances):
+        instance = instances.pop(0)
+        count -= 1
 
-    # Adjust concurrency based on GPU RAM (assuming medium.en model)
-    envs["CELERY_CONCURRENCY"] = str(floor(instance["gpu_ram"] / (7 * 1024)))
+        instance_id = instance["id"]
+        # Bid 1.5x the minimum bid
+        bid = round(float(instance["dph_total"]) * 1.5, 6)
 
-    # Set a nice hostname so we don't use a random Docker hash
-    git_commit = get_git_commit()
-    envs["CELERY_HOSTNAME"] = f"celery-{git_commit}@vast-{instance_id}"
+        # Adjust concurrency based on GPU RAM (assuming medium.en model)
+        envs["CELERY_CONCURRENCY"] = str(floor(instance["gpu_ram"] / (7 * 1024)))
 
-    body = {
-        "client_id": "me",
-        "image": image,
-        "args": ["worker"],
-        "env": envs,
-        "price": round(bid, 6),
-        "disk": 0.5,
-        "runtype": "args",
-    }
+        # Set a nice hostname so we don't use a random Docker hash
+        git_commit = get_git_commit()
+        envs["CELERY_HOSTNAME"] = f"celery-{git_commit}@vast-{instance_id}"
 
-    r = requests.put(
-        f"https://console.vast.ai/api/v0/asks/{instance_id}/",
-        params={"api_key": open(os.path.expanduser("~/.vast_api_key")).read().strip()},
-        json=body,
-    )
-    try:
+        body = {
+            "client_id": "me",
+            "image": image,
+            "args": ["worker"],
+            "env": envs,
+            "price": bid,
+            "disk": 0.5,
+            "runtype": "args",
+        }
+
+        r = requests.put(
+            f"https://console.vast.ai/api/v0/asks/{instance_id}/",
+            params={"api_key": vast_api_key},
+            json=body,
+        )
         r.raise_for_status()
-        return True
-    except requests.RequestException as e:
-        logging.error(e)
-        return False
+        logging.info(
+            f"Started instance {instance_id}, a {instance['gpu_name']} for ${bid}/hr"
+        )
 
 
-def delete_instance() -> bool:
-    deleted_running_instance = False
+def delete_instances(count: int):
     deletable_instances = []
 
     for instance in get_current_instances():
         if instance["actual_status"] == "exited":
-            deletable_instances.append(instance["id"])
-        elif not deleted_running_instance:
-            deletable_instances.append(instance["id"])
-            deleted_running_instance = True
+            deletable_instances.append(instance)
+        elif count:
+            deletable_instances.append(instance)
+            count -= 1
 
     if len(deletable_instances):
         for instance in deletable_instances:
-            p = subprocess.run(
-                ["vast", "destroy", "instance", "--raw", str(instance)],
-                capture_output=True,
+            r = requests.delete(
+                f"https://console.vast.ai/api/v0/instances/{instance['id']}/",
+                params={"api_key": vast_api_key},
+                json={},
             )
-            p.check_returncode()
-
-    return deleted_running_instance
+            r.raise_for_status()
+            logging.info(
+                f"Deleted instance {instance['id']}, had status: {instance['status_msg']}"
+            )
 
 
 def autoscale(
-    min_instances: int, max_instances: int, throughput: int, envs: dict, image: str
+    min_instances: int, max_instances: int, throughput: int, image: str
 ) -> int:
+    envs = dotenv_values(".env.vast")
+
     queue_status = get_queue_status(envs)
     current_instances = int(queue_status["consumers"])
     past_utilization.insert(0, float(queue_status["consumer_utilisation"]))
@@ -174,21 +163,29 @@ def autoscale(
     desired_instances = current_instances
     # TODO: make this a moving average/weighted average
     past_utilization_avg = mean(past_utilization)
-    if past_utilization_avg < 0.9:
+    if (
+        past_utilization_avg < 0.9
+        and queue_status["backing_queue_status"]["ack_egress_rate"]
+        < queue_status["backing_queue_status"]["ack_ingress_rate"]
+    ):
         message_count = queue_status["messages"]
         desired_instances += round(message_count / throughput)
-    elif past_utilization_avg == 1 and queue_status["backing_queue_status"]["avg_ack_egress_rate"] > queue_status["backing_queue_status"]["avg_ack_ingress_rate"]:
+    elif (
+        past_utilization_avg == 1
+        and queue_status["backing_queue_status"]["avg_ack_egress_rate"]
+        > queue_status["backing_queue_status"]["avg_ack_ingress_rate"]
+    ):
         desired_instances -= 1
 
     scale = (
         min(max_instances, max(min_instances, desired_instances)) - current_instances
     )
-    if abs(scale):
-        for _ in range(abs(scale)):
-            if scale > 0:
-                create_instance(envs, image)
-            else:
-                delete_instance()
+    count = abs(scale)
+    if count:
+        if scale > 0:
+            create_instances(count, envs, image)
+        else:
+            delete_instances(count)
 
     return scale
 
@@ -226,21 +223,19 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    envs = dotenv_values(".env.vast")
+    vast_api_key = os.getenv(
+        "VAST_API_KEY", open(os.path.expanduser("~/.vast_api_key")).read().strip()
+    )
 
     interval = 120
 
-    logging.info(f"Started autoscaler: min_instances={args.min_instances} max_instances={args.max_instances} throughput={args.throughput} image={args.image}")
+    logging.info(
+        f"Started autoscaler: min_instances={args.min_instances} max_instances={args.max_instances} throughput={args.throughput} image={args.image}"
+    )
 
     while True:
         start = time.time()
-        result = autoscale(
-            args.min_instances,
-            args.max_instances,
-            args.throughput,
-            envs,
-            args.image,
-        )
+        result = autoscale(**vars(args))
         logging.info(f"Workers change: {result}")
         end = time.time()
         time.sleep(interval - (end - start))
