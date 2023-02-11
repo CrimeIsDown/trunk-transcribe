f2aaaeef1cdc6bac2b1f96f48c4330ad863f3f38
Eric Tendian <erictendian@gmail.com>
2023-02-11 12:35:16 -0600

    Removing URL checking from entrypoint

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -Eeou pipefail
 if [ "$1" = 'api' ]; then
     exec uvicorn app.api:app --host 0.0.0.0
 elif [ "$1" = 'worker' ]; then
-    if [ -z "$CELERY_HOSTNAME" ]; then
+    if [ -z "${CELERY_HOSTNAME-}" ]; then
         CELERY_HOSTNAME="celery-${GIT_COMMIT::7}"
         if [ -f /root/.vast_containerlabel ]; then
             CELERY_HOSTNAME="$CELERY_HOSTNAME@$(cat /root/.vast_containerlabel)"
@@ -12,13 +12,6 @@ elif [ "$1" = 'worker' ]; then
             CELERY_HOSTNAME="$CELERY_HOSTNAME@%n"
         fi
     fi
-    # Make sure we can connect to these hosts
-    if [ -f .env ]; then
-        export $(grep -E '^(MEILI_URL|API_BASE_URL|S3_ENDPOINT)' .env | xargs)
-    fi
-    curl -sS "$MEILI_URL" > /dev/null
-    curl -sS "$API_BASE_URL" > /dev/null
-    curl -sS "$S3_ENDPOINT" > /dev/null
 
     exec celery --app=app.worker.celery worker \
         -P ${CELERY_POOL:-prefork} \
