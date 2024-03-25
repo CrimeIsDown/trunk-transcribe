0f0e426e1e363a03289bcb047f396532d7d3313b
Eric Tendian <erictendian@gmail.com>
2024-03-24 21:07:23 -0500

    Fixing warning

--- app/whisper.py
+++ app/whisper.py
@@ -144,10 +144,12 @@ class InsanelyFastWhisper(BaseWhisper):
         initial_prompt: str | None = None,
         **decode_options,
     ) -> WhisperResult:
+        # TODO: Find a way to support prompts, possibly following this https://github.com/huggingface/distil-whisper/issues/20#issuecomment-1823217041
+        generate_kwargs = {"task": "transcribe", "language": language}
         output = self.pipe(
-            audio, chunk_length_s=30, batch_size=24, return_timestamps=True
+            audio, chunk_length_s=30, batch_size=24, return_timestamps=True, generate_kwargs=generate_kwargs
         )
-        result: WhisperResult = {"segments": [], "text": output["text"]}
+        result: WhisperResult = {"segments": [], "text": output["text"], "language": language}
         for chunk in output["chunks"]:  # type: ignore
             result["segments"].append(
                 {
