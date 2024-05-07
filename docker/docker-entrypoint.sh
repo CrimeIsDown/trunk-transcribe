3a27687053c7f019deb32513ae99d09ad0deadf9
Eric Tendian <erictendian@gmail.com>
2024-05-06 23:09:59 -0500

    Fix errors

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -24,6 +24,8 @@ elif [ "$1" = 'worker' ]; then
         fi
     fi
 
+    export LD_LIBRARY_PATH=`python3 -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
+
     exec celery --app=app.worker.celery worker \
         -P ${CELERY_POOL:-prefork} \
         -c ${CELERY_CONCURRENCY:-1} \
