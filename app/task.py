3f022afa337ac1074594ae98bbca1a0662265e26
Eric Tendian <erictendian@gmail.com>
2024-09-22 01:30:46 -0500

    Do not auto retry WhisperException

--- app/task.py
+++ app/task.py
@@ -1,8 +1,11 @@
 from celery import Task as CeleryTask
 
+from app.whisper.exceptions import WhisperException
+
 
 class Task(CeleryTask):
     autoretry_for = (Exception,)
+    dont_autoretry_for = (WhisperException,)
     max_retries = 5
     retry_backoff = True
     retry_backoff_max = 600
