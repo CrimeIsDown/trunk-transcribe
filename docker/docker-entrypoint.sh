e31f62dab09dde9ee38001fd06f1585ff06adb7d
Eric Tendian <erictendian@gmail.com>
2025-01-02 01:34:20 -0600

    Switch from Poetry to uv

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ "$1" = 'api' ]; then
     /bin/sh -c "while true; do find /tmp -type f -mmin +10 -delete; sleep 60; done" &
     disown
 
-    exec poetry run uvicorn app.api:app --host 0.0.0.0 --log-level ${UVICORN_LOG_LEVEL:-info}
+    exec uv run uvicorn app.api:app --host 0.0.0.0 --log-level ${UVICORN_LOG_LEVEL:-info}
 elif [ "$1" = 'worker' ]; then
     # Clean up any old temp files
     /bin/sh -c "while true; do find /tmp -type f -mmin +10 -delete; sleep 60; done" &
@@ -24,16 +24,16 @@ elif [ "$1" = 'worker' ]; then
         fi
     fi
 
-    export LD_LIBRARY_PATH=`poetry run python3 -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
+    export LD_LIBRARY_PATH=`uv run python3 -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
 
-    exec poetry run celery --app=app.worker.celery worker \
+    exec uv run celery --app=app.worker.celery worker \
         -P ${CELERY_POOL:-prefork} \
         -c ${CELERY_CONCURRENCY:-1} \
         -l ${CELERY_LOGLEVEL:-info} \
         -n $CELERY_HOSTNAME \
         -Q ${CELERY_QUEUES:-transcribe,post_transcribe}
 elif [ "$1" = 'flower' ]; then
-    exec poetry run celery --app=app.worker.celery flower --port=5555
+    exec uv run celery --app=app.worker.celery flower --port=5555
 fi
 
 exec "$@"
