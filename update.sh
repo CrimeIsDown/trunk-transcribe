7a0794284afb8a779c4cb00418581e306b130c46
Eric Tendian <erictendian@gmail.com>
2025-01-05 19:24:53 -0600

    Optionally run DB migrations in update script

--- update.sh
+++ update.sh
@@ -7,6 +7,13 @@ git pull
 echo "The .env file settings have recently changed. Please ensure your COMPOSE_FILE variable has all the proper services enabled that you want. Refer to .env.example for more information."
 
 docker compose pull
+
+read -p "Would you like to run database migrations? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  docker compose run --rm api uv run alembic upgrade head
+fi
+
 docker compose up -d $(docker compose ps --services)
 
 # Optionally, reindex search calls if there was a change to the search schema
