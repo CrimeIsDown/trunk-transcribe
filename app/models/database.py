855eab04048b9854c21026c297569ec21a04f294
Eric Tendian <erictendian@gmail.com>
2025-01-09 00:34:58 -0600

    Quote DB passwords

--- app/models/database.py
+++ app/models/database.py
@@ -1,8 +1,9 @@
 import os
+from urllib.parse import quote_plus
 
 from sqlmodel import create_engine
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{os.getenv('POSTGRES_USER')}:{quote_plus(os.getenv('POSTGRES_PASSWORD'))}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
